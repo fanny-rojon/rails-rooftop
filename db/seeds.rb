81a2454d5ad090a5a894032f55ad31f371574f84
Fanny Rojon <fanny.rojon22@gmail.com>
2020-04-04 15:18:04 +0200

    created new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,21 +4,37 @@ Rooftop.destroy_all
 User.destroy_all
 
 puts "Creating Users..."
-fanny = User.create!(email: "email@example.com", password: "password", first_name: "Fanny", last_name: "Rojon")
-rui = User.create!(email: "email2@example.com", password: "password", first_name: "Rui", last_name: "Freitas")
-
+fanny = User.create!(email: "fanny@gmail.com", password: "password", first_name: "Fanny", last_name: "Rojon")
 
 puts "Creating Rooftops..."
-User.find_each do |user|
-  rand(2..5).times do
-    Rooftop.create!(
-      owner: user,
-      address: Faker::Address.full_address,
-      name: Faker::BossaNova.song,
-      description: Faker::Marketing.buzzwords,
-      price: rand(100..250)
-    )
-  end
+5.times do
+  Rooftop.create!(
+    owner: fanny,
+    address: Faker::Address.full_address,
+    name: Faker::TvShows::Friends.location,
+    description: Faker::TvShows::Friends.quote,
+    price: rand(100..250)
+  )
 end
 
 puts "Created #{User.count} user(s) and #{Rooftop.count} rooftop(s)"
+
+# puts "Creating Users..."
+# fanny = User.create!(email: "email@example.com", password: "password", first_name: "Fanny", last_name: "Rojon")
+# rui = User.create!(email: "email2@example.com", password: "password", first_name: "Rui", last_name: "Freitas")
+
+
+# puts "Creating Rooftops..."
+# User.find_each do |user|
+#   rand(2..5).times do
+#     Rooftop.create!(
+#       owner: user,
+#       address: Faker::Address.full_address,
+#       name: Faker::BossaNova.song,
+#       description: Faker::Marketing.buzzwords,
+#       price: rand(100..250)
+#     )
+#   end
+# end
+
+# puts "Created #{User.count} user(s) and #{Rooftop.count} rooftop(s)"
